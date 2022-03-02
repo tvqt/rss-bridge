bc2c93caee2fe02bddaad9123e531c7af7dacc52
Eugene Molotov <eugene.molotov@yandex.ru>
2022-03-02 22:36:06 +0500

    fixup! fixup! remove img inside video

--- bridges/InstagramBridge.php
+++ bridges/InstagramBridge.php
@@ -206,7 +206,7 @@ class InstagramBridge extends BridgeAbstract {
 
 	// returns Video post's contents and enclosures
 	protected function getInstagramVideoData($uri, $mediaURI, $mediaInfo, $textContent) {
-		$content = '<video controls src="proxy.php?' . urlencode($mediaInfo->video_url) . '" poster=proxy.php?"' . urlencode($mediaURI) . '" type="video/mp4">';
+		$content = '<video controls src="proxy.php?' . urlencode($mediaInfo->video_url) . '" poster="proxy.php?' . urlencode($mediaURI) . '" type="video/mp4">';
 		$content .= '</video><br>';
 		$content .= '<br>' . nl2br(htmlentities($textContent));
 
