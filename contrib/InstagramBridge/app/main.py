34c1e322916f2cf7346fb9e4a333d3a7f22099f5
Eugene Molotov <eugene.molotov@yandex.ru>
2022-08-19 23:24:15 +0500

    использую app для контроля браузера

---
+++ contrib/InstagramBridge/app/main.py
@@ -0,0 +1,116 @@
+import subprocess
+import logging
+import threading
+from time import sleep, time
+from werkzeug.exceptions import HTTPException
+from werkzeug.routing import Map, Rule
+from werkzeug.serving import run_simple
+from werkzeug.wrappers import Response
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+_logger = logging.getLogger(__name__)
+
+
+START_CRAWLING = True
+FIREFOX_PONGED = False
+
+
+class CrawlerThread(threading.Thread):
+    def run(self):
+        while True:
+            self._run()
+
+    def _run(self):
+        global START_CRAWLING
+        global FIREFOX_PONGED
+        filename = self._args[0]
+
+        while START_CRAWLING is False:
+            sleep(1)
+
+        try:
+            p = subprocess.Popen(['firefox', 'http://localhost:8028'])
+
+            instagram_users = []
+
+            with open(filename) as f:
+                instagram_users = sorted(set(filter(
+                    bool,
+                    map(lambda x: x.strip().lower(), f.readlines())
+                )))
+
+            _logger.info("%s users in text file" % len(instagram_users))
+
+            sleep(5)
+
+            for i, instagram_user in enumerate(instagram_users):
+                _logger.info("Progress: {} of {}".format(i+1, len(instagram_users)))
+                url = "https://www.instagram.com/" + instagram_user
+                _logger.info("Opening {}".format(url))
+                subprocess.Popen(['firefox', url])
+
+                start_time = time()
+                while True:
+                    if FIREFOX_PONGED is True:
+                        break
+
+                    elif time() - start_time > 60:
+                        _logger.warning("Killing firefox process")
+                        p.kill()
+                        sleep(30)
+                        _logger.info("starting new firefox")
+                        p = subprocess.Popen(['firefox', 'http://localhost:8028'])
+                        break
+
+                    sleep(1)
+
+                FIREFOX_PONGED = False
+
+        except Exception:
+            _logger.exception("Error in thread. Stopping crawling")
+            sleep(5)
+        finally:
+            p.kill()
+
+        START_CRAWLING = False
+        FIREFOX_PONGED = False
+
+
+url_map = Map([
+    Rule("/crawling/start", endpoint="start"),
+    Rule("/crawling/pong", endpoint="pong"),
+])
+
+
+def application(environ, start_response):
+    global START_CRAWLING
+    global FIREFOX_PONGED
+
+    try:
+        urls = url_map.bind_to_environ(environ)
+        endpoint, args = urls.match()
+
+        if endpoint == "start":
+            START_CRAWLING = True
+        elif endpoint == "pong":
+            FIREFOX_PONGED = True
+
+        response = Response("ok", mimetype="text/plain")
+    except HTTPException as e:
+        response = e.get_response(environ)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    return response(environ, start_response)
+
+
+if __name__ == "__main__":
+    crawler_thread = CrawlerThread(args=["../../../instagram_accounts.txt"])
+    crawler_thread.start()
+    run_simple("127.0.0.1", 8028, application, threaded=True)
---
+++ contrib/InstagramBridge/smalldonor.user.js
@@ -0,0 +1,239 @@
+// ==UserScript==
+// @name     Instagram content donor for RSS-Bridge
+// @version  1
+// @include  https://www.instagram.com/*
+// @grant    GM.xmlHttpRequest
+// @grant    GM.getValue
+// @grant    GM.setValue
+// ==/UserScript==
+
+const ACCESS_TOKEN = 'test_token';
+const RSSBRIDGE_ROOT='http://localhost:82';
+const INSTAGRAM_ACCOUNTS_URL=RSSBRIDGE_ROOT + '/instagram_accounts.txt';
+const APP_ROOT='http://localhost:8028';
+
+/*
+Example:
+const LOGINS_PASSWORDS = [
+  "username1 password1",
+  "username2 password2",
+  "username3 password3",
+];
+*/
+const LOGINS_PASSWORDS = [
+];
+
+function random_choise(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+function sleep(s) {
+  let ms = 1000*s;
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function get(url) {
+  return new Promise((resolve, reject) => {
+    GM.xmlHttpRequest({
+      method: "GET",
+      url,
+      onload: function(response) {
+        if (response.status != 200) {
+          reject(response);
+        } else {
+          resolve(response);
+        }
+      },
+      onerror: reject
+    });
+  });
+}
+
+function post(url, data) {
+  return new Promise((resolve, reject) => {
+    GM.xmlHttpRequest({
+      method: "POST",
+      url,
+      headers: { "Content-type" : "application/x-www-form-urlencoded" },
+      data: data,
+      onload: function(response) {
+        if (response.status != 200) {
+          reject(response);
+        } else {
+          resolve(response);
+        }
+      },
+      onerror: reject
+    });
+  });
+}
+
+function setState(state) {
+  localStorage.setItem("donor_state", state);
+}
+
+function getState() {
+  return localStorage.getItem("donor_state") || "fetch_instagram_account";
+}
+
+var webProfileInfo;
+var webProfileInfoStatus;
+var _isLoggedIn;
+
+if (!unsafeWindow.XMLHttpRequest.prototype.getResponseText) {
+  unsafeWindow.XMLHttpRequest.prototype.getResponseText = Object.getOwnPropertyDescriptor(unsafeWindow.XMLHttpRequest.prototype, 'responseText').get;
+}
+Object.defineProperty(unsafeWindow.XMLHttpRequest.prototype, 'responseText', {
+  get: exportFunction(function() {
+    var responseText = unsafeWindow.XMLHttpRequest.prototype.getResponseText.call(this);
+    if (this.responseURL.startsWith("https://i.instagram.com/api/v1/users/web_profile_info/?username=")) {
+      webProfileInfo = responseText;
+      webProfileInfoStatus = this.status;
+    } else if (this.responseURL.startsWith("https://www.instagram.com/accounts/get_encrypted_credentials/")) {
+      _isLoggedIn = true;
+    }
+    return responseText;
+  }, unsafeWindow),
+  enumerable: true,
+  configurable: true
+});
+
+
+async function logout() {
+  let ili = await isLoggedIn();
+  if (!ili) return;
+  var s = document.createElement("script");
+  s.src = "https://www.instagram.com/accounts/logout";
+  document.head.appendChild(s);
+}
+
+async function isLoggedIn() {
+  console.log("checking if logged in");
+  for (var i=0; i<20; i++) {
+    if (_isLoggedIn) {
+      console.log("user is logged in");
+      return true;
+    }
+    await sleep(1);
+  }
+  console.log("user is NOT logged in");
+  return false;
+}
+
+function is429Error() {
+  if (webProfileInfoStatus == 429) return true;
+  let c = document.querySelector(".error-container");
+  return c && c.innerText.indexOf("Please wait") > -1;
+}
+
+async function main() {
+  while(!document || !document.querySelector) {
+    await sleep(1);
+  }
+
+  const state = getState();
+  console.log("current state", state);
+  switch(state) {
+  case "login":
+    await sleep(10);
+    if (await isLoggedIn()) {
+      setState("fetch_instagram_account");
+      location.pathname = "/";
+      return;
+    }
+    let loginBtns = Array.from(document.querySelectorAll("button[type='button']")).filter( x => x.innerText == "Log In" );
+    if (loginBtns.length) {
+      random_choise(loginBtns).click();
+    } else {
+      const [username_to_login, password] = await GM.getValue("lw");
+      if (!username_to_login || !password) {
+        alert("No login given");
+        return;
+      }
+      await sleep(3);
+      var el = null;
+      el = document.querySelector("input[name='username']");
+      if (!el) {
+        console.log("could not find username textbox. Redirecting");
+        await sleep(3);
+        location.pathname = "/accounts/login";
+        return;
+      }
+      document.querySelector("input[name='username']").focus();
+      document.execCommand("selectAll");
+      document.execCommand("insertText", false, username_to_login);
+      document.querySelector("input[name='password']").focus();
+      document.execCommand("selectAll");
+      document.execCommand("insertText", false, password);
+      document.querySelector("button[type='submit']").click();
+      setState("fetch_instagram_account");
+    }
+
+    // givin time to login, it will redirect automatically
+    await sleep(10);
+
+    // it could not login
+    setState("login");
+    alert("DONOR ERROR: Could not login");
+    await sleep(5);
+    location.reload();
+    break;
+
+  case "fetch_instagram_account":
+    let re = /[^/]+/;
+    let match = location.pathname.match(re);
+    if (!match || match.length > 1) {
+      alert("Not on user's page");
+      return;
+    }
+    let username = match[0];
+
+    await sleep(10 + 5 * Math.random()); // give time to fetch webProfileInfo
+    for(let i=0; i<30; i++) {
+      if (webProfileInfoStatus > 0) break;
+      await sleep(1);
+    }
+
+    if (is429Error()) {
+      logout();
+      setState("login"); // TODO: should not wait for time
+      location.pathname = "/";
+      return;
+    }
+
+    try {
+      const sharedData = unsafeWindow._sharedData;
+      if (sharedData && sharedData.entry_data && Object.keys(sharedData.entry_data).length > 0) {
+        let r = await post(
+          RSSBRIDGE_ROOT + "/?action=cache&bridge=Instagram&as_json=1&key=instagram_user_" + username,
+          "value=" + encodeURIComponent(JSON.stringify(sharedData)) + "&access_token=" + encodeURIComponent(ACCESS_TOKEN)
+        );
+      } else if (webProfileInfo) {
+        let r = await post(
+          RSSBRIDGE_ROOT + "/?action=cache&bridge=Instagram&as_json=1&key=instagram_user_" + username,
+          "value=" + encodeURIComponent(webProfileInfo) + "&access_token=" + encodeURIComponent(ACCESS_TOKEN)
+        );
+      }
+    } catch(e) {
+      console.error("DONOR ERROR: error while posting cache", e);
+      await sleep(10);
+      location.reload();
+      return;
+    }
+
+    window.scrollTo({"top": 500, "left": 0, "behavior": "smooth"});
+    await sleep(1 + 3 * Math.random());
+    document.elementFromPoint(400, 100).click();
+    await sleep(3 + 3 * Math.random());
+
+    await post(APP_ROOT + "/crawling/pong");
+    break;
+
+  default:
+    alert("Unknown state: " + state);
+    break;
+  };
+};
+
+main();
